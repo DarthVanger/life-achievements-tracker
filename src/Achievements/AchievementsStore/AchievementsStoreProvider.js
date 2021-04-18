85130086d80e068d2519aca4b008f7af82301136
DarthVanger <darthvanger@gmail.com>
2021-04-19 01:02:02 +0300

    Expose errors from storage provider

--- src/Achievements/AchievementsStore/AchievementsStoreProvider.js
+++ src/Achievements/AchievementsStore/AchievementsStoreProvider.js
@@ -18,33 +18,55 @@ const initialAchievements = [
 
 const AchievementsStoreProvider = ({ children }) => {
   const [isLoaded, setIsLoaded] = useState(false);
+  const [errorLoading, setErrorLoading] = useState(false);
+  const [errorSaving, setErrorSaving] = useState(false);
   const [achievements, setAchievements] = useState([]);
 
   useEffect(() => {
     if (isLoaded) {
-      persistentStorage.saveAchievements(achievements);
-      console.log('Achievements saved to persistent storage')
+      (async () => {
+        try {
+          await persistentStorage.saveAchievements(achievements);
+          console.log('Achievements saved to persistent storage')
+        } catch (e) {
+          console.log('Failed to save achievements to persistent storage')
+          setErrorSaving(true);
+        }
+      })();
     }
   }, [achievements]);
 
   useEffect(() => {
     (async () => {
-      const persistedAchievements = await persistentStorage.getAchievements();
-      console.log('Loaded achievements from persistent storage: ', persistedAchievements)
+      try {
+        const persistedAchievements = await persistentStorage.getAchievements();
+        console.log('Loaded achievements from persistent storage: ', persistedAchievements)
 
-      setIsLoaded(true);
+        setIsLoaded(true);
 
-      if (persistedAchievements === null) {
-        console.log('Saving initial achivements list');
-        setAchievements(initialAchievements);
-      } else {
-        setAchievements(persistedAchievements);
+        if (persistedAchievements === null) {
+          console.log('Saving initial achivements list');
+          setAchievements(initialAchievements);
+        } else {
+          setAchievements(persistedAchievements);
+        }
+      } catch (e) {
+        console.log('Failed to load achievements from persistent storage')
+        setErrorLoading(true);
       }
     })();
   }, []);
 
+  const store = {
+    achievements,
+    setAchievements,
+    isLoaded,
+    errorLoading,
+    errorSaving,
+  };
+
   return (
-    <AchievementsContext.Provider value={{ achievements, setAchievements }}>
+    <AchievementsContext.Provider value={store}>
       {children}
     </AchievementsContext.Provider>
   );
--- src/Achievements/AchievementsStore/phoneStorage.js
+++ src/Achievements/AchievementsStore/phoneStorage.js
@@ -3,22 +3,14 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 const storageKey = 'achievements';
 
 const saveAchievements = async (achievements) => {
-  try {
-    const jsonValue = JSON.stringify(achievements)
-    await AsyncStorage.setItem(storageKey, jsonValue)
-  } catch (e) {
-    // TODO: notify user
-  }
+  const jsonValue = JSON.stringify(achievements)
+  await AsyncStorage.setItem(storageKey, jsonValue)
 }
 
 const getAchievements = async () => {
-  try {
-    const value = await AsyncStorage.getItem(storageKey)
-    const jsonValue = JSON.parse(value)
-    return jsonValue;
-  } catch (e) {
-    // TODO: notify user
-  }
+  const value = await AsyncStorage.getItem(storageKey)
+  const jsonValue = JSON.parse(value)
+  return jsonValue;
 }
 
 export { saveAchievements, getAchievements };
